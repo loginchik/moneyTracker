bac46de84863d5d3a8556195525a6be8e3c2605b
Anna Loginova <zv1376a@gmail.com>
2024-01-07 05:22:32 +0300

    Config update

--- bot/config.py
+++ bot/config.py
@@ -5,13 +5,6 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     app_name: str = 'Spendings Manager'
     bot_token: SecretStr
-    postgresql_url: SecretStr
-    db_host: SecretStr
-    db_port: int
-    db_user: SecretStr
-    db_password: SecretStr
-    db_name: SecretStr
-
     db_url: SecretStr
     db_apikey: SecretStr
     model_config = SettingsConfigDict(env_file='.env', 
